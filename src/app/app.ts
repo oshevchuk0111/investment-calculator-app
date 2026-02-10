3412ccfd67acc4dc4c93712156f5a6dc38989c1e
oshevchuk0111 <o.shevchuk0111@gmail.com>
2026-02-10 19:57:49 +0100

    add header

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { HeaderComponent } from './header/header';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class AppComponent {
+  protected readonly title = signal('investment-calculator-app');
+}
